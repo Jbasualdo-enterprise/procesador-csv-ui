611eb7f88b25219be7412d2de39ba4b6db5fb7fc
jbasualdo <jbasualdo@irsacorp.com.ar>
2025-07-23 11:21:07 -0300

    Setup outDir to docs

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/procesador-csv-ui/'
+  base: '/procesador-csv-ui/',
+  build: { outDir: 'docs' }
 })
